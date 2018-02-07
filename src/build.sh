df9aa059e80ee351fa7893acaa3d94f0451df4d5
alexfranz <ut1339@gmail.com>
2018-02-07 09:43:58 +0300

    ALL-00: scripts were updated

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-7z a -t7z takagi-san.7z -ir@"./$1building_files.txt"
+if [ -z "$1" ]
+then
+      7z a -t7z takagi-san.7z -ir@"./src/building_files.txt"
+else
+      7z a -t7z takagi-san.7z -ir@"./$1building_files.txt"
+fi
--- src/naming.sh
+++ src/naming.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-sed -i -e 's/Tanabe/Танабэ/g' $1.ass
-sed -i -e 's/Nishikata/Нисиката/g' $1.ass
-sed -i -e 's/Takagi/Такаги/g' $1.ass
-sed -i -e 's/Mina/Мина/g' $1.ass
-sed -i -e 's/Yukari/Юкари/g' $1.ass
-sed -i -e 's/Sanae/Санаэ/g' $1.ass
-sed -i -e 's/Nakai/Накай/g' $1.ass
-sed -i -e 's/-san/-сан/g' $1.ass
-sed -i -e 's/-chan/-тян/g' $1.ass
-sed -i -e 's/-kun/-кун/g' $1.ass
-sed -i -e 's/-sensei/-сенсей/g' $1.ass
-rm -rf $1.ass-e
+VAR1 = $1
+VAR1 = ${VAR1:="*"}
+
+sed -i -e 's/Tanabe/Танабэ/g' $VAR1.ass
+sed -i -e 's/Nishikata/Нисиката/g' $VAR1.ass
+sed -i -e 's/Takagi/Такаги/g' $VAR1.ass
+sed -i -e 's/Mina/Мина/g' $VAR1.ass
+sed -i -e 's/Yukari/Юкари/g' $VAR1.ass
+sed -i -e 's/Sanae/Санаэ/g' $VAR1.ass
+sed -i -e 's/Nakai/Накай/g' $VAR1.ass
+sed -i -e 's/-san/-сан/g' $VAR1.ass
+sed -i -e 's/-chan/-тян/g' $VAR1.ass
+sed -i -e 's/-kun/-кун/g' $VAR1.ass
+sed -i -e 's/-sensei/-сенсей/g' $VAR1.ass
+rm -rf $VAR1.ass-e
