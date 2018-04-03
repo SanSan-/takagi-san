39d49030f1b52f6054ee812b4c962d6fc5f2a2a8
alexfranz <ut1339@gmail.com>
2018-04-03 15:02:17 +0300

    ep-05: init

--- src/naming.sh
+++ src/naming.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-MASK = $1
-MASK = ${MASK:="*"}
-
-sed -i -e 's/Tanabe/Танабэ/g' $MASK.ass
-sed -i -e 's/Nishikata/Нисиката/g' $MASK.ass
-sed -i -e 's/Takagi/Такаги/g' $MASK.ass
-sed -i -e 's/Mina/Мина/g' $MASK.ass
-sed -i -e 's/Yukari/Юкари/g' $MASK.ass
-sed -i -e 's/Sanae/Санаэ/g' $MASK.ass
-sed -i -e 's/Nakai/Накай/g' $MASK.ass
-sed -i -e 's/Mano/Мано/g' $MASK.ass
-sed -i -e 's/Takao/Такао/g' $MASK.ass
-sed -i -e 's/-san/-сан/g' $MASK.ass
-sed -i -e 's/-chan/-тян/g' $MASK.ass
-sed -i -e 's/-kun/-кун/g' $MASK.ass
-sed -i -e 's/-sensei/-сенсей/g' $MASK.ass
-rm -rf $MASK.ass-e
+#sed -i -e 's/Tanabe/Танабэ/g' $1.ass
+#sed -i -e 's/Nishikata/Нисиката/g' $1.ass
+#sed -i -e 's/Takagi/Такаги/g' $1.ass
+#sed -i -e 's/Mina/Мина/g' $1.ass
+#sed -i -e 's/Yukari/Юкари/g' $1.ass
+#sed -i -e 's/Sanae/Санаэ/g' $1.ass
+#sed -i -e 's/Nakai/Накай/g' $1.ass
+#sed -i -e 's/Mano/Мано/g' $1.ass
+sed -i -e 's/Takao/Такао/g' $1.ass
+sed -i -e 's/Kimura/Кимура/g' $1.ass
+sed -i -e 's/Sakurai/Сакурай/g' $1.ass
+#sed -i -e 's/-san/-сан/g' $1.ass
+#sed -i -e 's/-chan/-тян/g' $1.ass
+#sed -i -e 's/-kun/-кун/g' $1.ass
+#sed -i -e 's/-sensei/-сенсей/g' $1.ass
+rm -rf $1.ass-e
--- src/restyle.sh
+++ src/restyle.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-MASK = $1
-MASK = ${MASK:="*"}
-
-sed -i -e 's/,Flashback Italics,/,Italics,/g' $MASK.ass
-sed -i -e 's/,Flashback,/,Main,/g' $MASK.ass
-sed -i -e 's/,Main,Танабэ/,Main-Tanabe,Танабэ/g' $MASK.ass
-sed -i -e 's/,Main,Нисиката/,Main-Nishikata,Нисиката/g' $MASK.ass
-sed -i -e 's/,Main Top,Нисиката,0000,0000,0000,,/,Main-Nishikata,Нисиката,0000,0000,0000,,{\\an8}/g' $MASK.ass
-sed -i -e 's/,Italics,Нисиката,0000,0000,0000,,/,Main-Nishikata,Нисиката,0000,0000,0000,,{\\i1}/g' $MASK.ass
-sed -i -e 's/,Italics Top,Нисиката,0000,0000,0000,,/,Main-Nishikata,Нисиката,0000,0000,0000,,{\\an8\\i1}/g' $MASK.ass
-sed -i -e 's/,Main Top,Нисиката,0,0,0,,/,Main-Nishikata,Нисиката,0,0,0,,{\\an8}/g' $MASK.ass
-sed -i -e 's/,Italics,Нисиката,0,0,0,,/,Main-Nishikata,Нисиката,0,0,0,,{\\i1}/g' $MASK.ass
-sed -i -e 's/,Italics Top,Нисиката,0,0,0,,/,Main-Nishikata,Нисиката,0,0,0,,{\\an8\\i1}/g' $MASK.ass
-sed -i -e 's/,Main,Такаги/,Main-Takagi,Такаги/g' $MASK.ass
-sed -i -e 's/,Main,Мина/,Main-Mina,Мина/g' $MASK.ass
-sed -i -e 's/,Main,Юкари/,Main-Yukari,Юкари/g' $MASK.ass
-sed -i -e 's/,Main,Санаэ/,Main-Sanae,Санаэ/g' $MASK.ass
-sed -i -e 's/,Main,Накай/,Main-Nakai,Накай/g' $MASK.ass
-sed -i -e 's/,Main,Мано/,Main-Mano,Мано/g' $MASK.ass
-sed -i -e 's/,Main,Такао/,Main-Takao,Такао/g' $MASK.ass
-rm -rf $MASK.ass-e
+sed -i -e 's/,Flashback Italics,/,Italics,/g' $1.ass
+sed -i -e 's/,Flashback,/,Main,/g' $1.ass
+sed -i -e 's/,Main,Танабэ/,Main-Tanabe,Танабэ/g' $1.ass
+sed -i -e 's/,Main,Нисиката/,Main-Nishikata,Нисиката/g' $1.ass
+sed -i -e 's/,Main Top,Нисиката,0000,0000,0000,,/,Main-Nishikata,Нисиката,0000,0000,0000,,{\\an8}/g' $1.ass
+sed -i -e 's/,Italics,Нисиката,0000,0000,0000,,/,Main-Nishikata,Нисиката,0000,0000,0000,,{\\i1}/g' $1.ass
+sed -i -e 's/,Italics Top,Нисиката,0000,0000,0000,,/,Main-Nishikata,Нисиката,0000,0000,0000,,{\\an8\\i1}/g' $1.ass
+sed -i -e 's/,Main Top,Нисиката,0,0,0,,/,Main-Nishikata,Нисиката,0,0,0,,{\\an8}/g' $1.ass
+sed -i -e 's/,Italics,Нисиката,0,0,0,,/,Main-Nishikata,Нисиката,0,0,0,,{\\i1}/g' $1.ass
+sed -i -e 's/,Italics Top,Нисиката,0,0,0,,/,Main-Nishikata,Нисиката,0,0,0,,{\\an8\\i1}/g' $1.ass
+sed -i -e 's/,Main,Такаги/,Main-Takagi,Такаги/g' $1.ass
+sed -i -e 's/,Main Top,Такаги,0,0,0,,/,Main-Takagi,Такаги,0,0,0,,{\\an8}/g' $1.ass
+sed -i -e 's/,Main,Мина/,Main-Mina,Мина/g' $1.ass
+sed -i -e 's/,Main,Юкари/,Main-Yukari,Юкари/g' $1.ass
+sed -i -e 's/,Italics,Юкари,0,0,0,,/,Main-Yukari,Юкари,0,0,0,,{\\i1}/g' $1.ass
+sed -i -e 's/,Main,Санаэ/,Main-Sanae,Санаэ/g' $1.ass
+sed -i -e 's/,Main,Накай/,Main-Nakai,Накай/g' $1.ass
+sed -i -e 's/,Main,Мано/,Main-Mano,Мано/g' $1.ass
+sed -i -e 's/,Main,Такао/,Main-Takao,Такао/g' $1.ass
+sed -i -e 's/,Main,Кимура/,Main-Kimura,Кимура/g' $1.ass
+sed -i -e 's/,Main,Сакурай/,Main-Sakurai,Сакурай/g' $1.ass
+rm -rf $1.ass-e
