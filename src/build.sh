b11e2458390298ea6b7710d0a6c990f450b0051f
Alex Franz <sansan@kashenka.ru>
2020-05-05 02:16:09 +0300

    - build script was updated: SC1007 Remove space - extract script was updated: dest folder updated

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FOLDER = $1
-FOLDER = ${FOLDER:="src/"}
+FOLDER=$1
+FOLDER=${FOLDER:="src/"}
 
 7z a -t7z takagi-san.7z -ir@"./"$FOLDER"building_files.txt"
--- src/extract.sh
+++ src/extract.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-"/Applications/MKVToolNix-9.4.0.app/Contents/MacOS/mkvmerge" -i $1.mkv
-"/Applications/MKVToolNix-9.4.0.app/Contents/MacOS/mkvextract" tracks $1.mkv 2:$1.ass
+"/Applications/MKVToolNix-46.0.0.app/Contents/MacOS/mkvmerge" -i $1.mkv
+"/Applications/MKVToolNix-46.0.0.app/Contents/MacOS/mkvextract" tracks $1.mkv 2:$1.ass
